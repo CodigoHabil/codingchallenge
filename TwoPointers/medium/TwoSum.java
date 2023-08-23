79979ab58f7cc2b6cab7226de430094e7d9dc234
iCharlieAraiza <eliptikman1234@gmail.com>
2023-08-22 22:37:59 -0600

    Create TwoSum.java

---
+++ TwoPointers/medium/TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = 0;
+
+        int i = 0;
+        int j = 0;
+
+        for(int k = 0; k < prices.length; k++) {
+            if(prices[k] > ) {
+                max = prices[k];
+                i = k;
+            }
+        }
+        return max;
+    }
+}
