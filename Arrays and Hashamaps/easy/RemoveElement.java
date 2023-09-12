2a1b69233f869954531e0d69456f1732b8d7320e
iCharlieAraiza <eliptikman1234@gmail.com>
2023-09-11 21:11:03 -0600

    Update RemoveElement.java

--- Arrays and Hashamaps/easy/RemoveElement.java
+++ Arrays and Hashamaps/easy/RemoveElement.java
@@ -1,14 +1,11 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        int[] aux = new int[nums.length];
-        int j = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != val) {
-                nums[j++] = nums[i];
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int j = i + 1; j < nums.length; j++ ) {
+            if(nums[i] != nums[j]) {
+                nums[++i] = nums[j];
             }
         }
-
-        return j;
+        return i + 1;
     }
 }
